2235861f480c14452de0d45d34eee5c145c80ba1
Noah Stolmaker <nstolmaker@gmail.com>
2019-10-30 20:27:23 -0700

    Permission handling that may or may not be necessary now that i have added Write permissions to the player settings for the android build. Its hooked in to the LoadSongInfos. So it could be tested by pulling that out and seeing if it will let you load a song still or not

---
+++ Assets/PermissionHandling.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Android;
+
+public class PermissionHandling : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("NOAHDEBUG Checking for permissions");
+        if (Permission.HasUserAuthorizedPermission(Permission.ExternalStorageRead))
+        {
+            // The user authorized use of the microphone.
+        }
+        else
+        {
+            // We do not have permission to use the microphone.
+            // Ask for permission or proceed without the functionality enabled.
+            Permission.RequestUserPermission(Permission.ExternalStorageRead);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
